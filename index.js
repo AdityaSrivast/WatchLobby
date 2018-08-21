675932036184e9f3885eeab436e45575a3bbed3d
adityasrivast <yashsri421@gmail.com>
2018-08-22 02:15:57 +0530

    add background

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var server = require('http').createServer(app);
 var io=require('socket.io')(server);
 
 app.set('view engine','ejs');
-app.use(express.static(__dirname+'/public'))
+// app.use(express.static(__dirname+'/public'))
+app.use(express.static('public'))
+
 
 app.get('/',function(req,res){
     res.render('index');
